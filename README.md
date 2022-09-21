f6f17fc4c7f3cdb765bfb2c156f67b3de5dd02d4
scirin <irakokakola@gmail.com>
2022-09-21 06:25:38 +0000

    Fixed exception handling

--- README.md
+++ README.md
@@ -11,11 +11,10 @@ A minimal subset of OS with compiled bootloader and simple kernel
 To use RustyOS you must have the following components installed:
 * QEMU - Virtualization technology, check https://www.qemu.org/ for installation guide
 
-To install Bootimage and supporting tools, run following commands
+To install Bootimage and supporting tools, run following commands:
 * cargo install cargo-binutils
 * cargo install bootimage
 * rustup toolchain install nightly
 * rustup default nightly
 * rustup component add rust-src
 * rustup component add llvm-tools-preview
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]                                                              // Run the program without OS
 #![no_main]
 #![feature(core_intrinsics)]                                            // Use LLVM intrinsic functions
+#![feature(lang_items)]
 
 use core::intrinsics;
 use core::panic::PanicInfo;                                             // Allow panic handler to inspect where panic occured
@@ -14,18 +15,20 @@ pub fn panic(_info: &PanicInfo) -> ! {
     }                                                                   // Crash
 }
 
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() { }
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let framebuffer = 0xb8000 as *mut u8;
-
     unsafe {
         framebuffer
             .offset(1)                                                 // Increment pointer address by 1 to 0xb8001 
             .write_volatile(0xC0);                                     // Add color
     }
-
     loop{
-        hlt();                                                         // Halt instruction for CPU to save electricity
+        hlt();                                                         // Halt instruction for CPU to save electricity 
     }
 }
 
