e9cdea2ed8902bb262cbddb100c678a37c25242e
scirin <irakokakola@gmail.com>
2022-09-22 12:04:50 +0000

    Added VGA colors

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,31 @@ pub extern "C" fn _start() -> ! {
     unsafe {
         framebuffer
             .offset(1)                                                 // Increment pointer address by 1 to 0xb8001 
-            .write_volatile(0xC0);                                     // Add color
+            .write_volatile(0x30);                                     // Add color
     }
     loop{
         hlt();                                                         // Halt instruction for CPU to save electricity 
     }
 }
 
-
+#[allow(unused)]                                                       // Represents a 16 bit color used for vga display.
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum Color16 {
+    Black   =   0x0,
+    Blue    =   0x1,
+    Green   =   0x2,
+    Cyan    =   0x3,
+    Red     =   0x4,
+    Magenta =   0x5,
+    Brown   =   0x6,
+    Pink    =   0xD,
+    Yellow  =   0xE,
+    White   =   0xF,
+    LightGrey   =   0x7,
+    DarkGrey    =   0x8,
+    LightBlue   =   0x9,
+    LightGreen  =   0xA,
+    LightCyan   =   0xB,
+    LightRed    =   0xC,
+}
